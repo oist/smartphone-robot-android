70a1ede028860aa2615b457150c642afe1581ad7
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-03-19 15:50:36 +0900

    Initial setup of Image Analyzer from Camera X

--- apps/camera/src/main/java/jp/oist/abcvlib/camera/MainActivity.java
+++ apps/camera/src/main/java/jp/oist/abcvlib/camera/MainActivity.java
@@ -3,12 +3,16 @@ package jp.oist.abcvlib.camera;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -19,6 +23,8 @@ import androidx.lifecycle.LifecycleOwner;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 public class MainActivity extends AppCompatActivity implements LifecycleOwner {
 
@@ -27,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements LifecycleOwner {
 
     private ListenableFuture<ProcessCameraProvider> mCameraProviderFuture;
     private PreviewView mPreviewView;
+
+    private ExecutorService analysisExecutor;
 
 
     @Override
@@ -42,14 +50,34 @@ public class MainActivity extends AppCompatActivity implements LifecycleOwner {
             ActivityCompat.requestPermissions(
                     this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
+
+        int threadPoolSize = 4;
+        analysisExecutor = new ScheduledThreadPoolExecutor(threadPoolSize);
     }
 
-    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+    private void bindAll(@NonNull ProcessCameraProvider cameraProvider) {
         Preview preview = new Preview.Builder().build();
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
                 .build();
-        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+        imageAnalysis.setAnalyzer(analysisExecutor, new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                int rotationDegrees = image.getImageInfo().getRotationDegrees();
+                // insert your code here.
+                Log.i("analyzer", "Image roatation: " + rotationDegrees);
+                image.close();
+            }
+        });
+
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis);
         preview.setSurfaceProvider(mPreviewView.getSurfaceProvider());
     }
 
@@ -59,7 +87,7 @@ public class MainActivity extends AppCompatActivity implements LifecycleOwner {
             mCameraProviderFuture.addListener(() -> {
                 try {
                     ProcessCameraProvider cameraProvider = mCameraProviderFuture.get();
-                    bindPreview(cameraProvider);
+                    bindAll(cameraProvider);
                 } catch (ExecutionException | InterruptedException e) {
                     // No errors need to be handled for this Future.
                     // This should never be reached.
