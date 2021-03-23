c64e74c348650a7b99c9c23e236e59c6e0a245a9
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-03-23 17:09:50 +0900

    Created new module for server learning and reduced camera module to bare min

--- apps/camera/src/main/java/jp/oist/abcvlib/camera/MainActivity.java
+++ apps/camera/src/main/java/jp/oist/abcvlib/camera/MainActivity.java
@@ -1,50 +1,20 @@
 package jp.oist.abcvlib.camera;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import jp.oist.abcvlib.core.AbcvlibActivity;
-import jp.oist.abcvlib.core.outputs.SocketListener;
 
 
-public class MainActivity extends AbcvlibActivity implements SocketListener {
+public class MainActivity extends AbcvlibActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         // Setup a live preview of camera feed to the display. Remove if unwanted. 
-        setContentView(jp.oist.abcvlib.core.R.layout.camera_x_preview);
+        setContentView(R.layout.camera_x_preview);
 
-        switches.pythonControlledPIDBalancer = true;
         switches.cameraXApp = true;
-        initialzer(this, "192.168.28.233", 3000, null, this);
+        initialzer(this);
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onServerReadSuccess(JSONObject msgFromServer) {
-        // Parse Message from Server
-        // ..
-        Log.i("server", msgFromServer.toString());
-
-        // Send return message
-        sendToServer();
-    }
-
-    private void sendToServer(){
-        JSONObject msgToServer = new JSONObject();
-
-        try{
-            msgToServer.put("Name1", "Value1");
-            msgToServer.put("Name2", "Value2");
-            // Add as many JSON subobjects as you need here.
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        this.outputs.socketClient.writeInputsToServer(msgToServer);
     }
 }
---
+++ apps/serverLearning/src/main/java/jp/oist/abcvlib/serverlearning/MainActivity.java
@@ -0,0 +1,50 @@
+package jp.oist.abcvlib.serverlearning;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import jp.oist.abcvlib.core.AbcvlibActivity;
+import jp.oist.abcvlib.core.outputs.SocketListener;
+
+
+public class MainActivity extends AbcvlibActivity implements SocketListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Setup a live preview of camera feed to the display. Remove if unwanted. 
+        setContentView(jp.oist.abcvlib.core.R.layout.camera_x_preview);
+
+        switches.pythonControlledPIDBalancer = true;
+        switches.cameraXApp = true;
+        initialzer(this, "192.168.28.233", 3000, null, this);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onServerReadSuccess(JSONObject msgFromServer) {
+        // Parse Message from Server
+        // ..
+        Log.i("server", msgFromServer.toString());
+
+        // Send return message
+        sendToServer();
+    }
+
+    private void sendToServer(){
+        JSONObject msgToServer = new JSONObject();
+
+        try{
+            msgToServer.put("Name1", "Value1");
+            msgToServer.put("Name2", "Value2");
+            // Add as many JSON subobjects as you need here.
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        this.outputs.socketClient.writeInputsToServer(msgToServer);
+    }
+}
