11745a12c3f9f3eebac6be1bf35252addc4f9e4b
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-04-06 13:27:09 +0900

    added basic test code for episode classes in java

--- apps/serverLearning/src/main/java/jp/oist/abcvlib/serverlearning/MainActivity.java
+++ apps/serverLearning/src/main/java/jp/oist/abcvlib/serverlearning/MainActivity.java
@@ -44,10 +44,6 @@ import jp.oist.abcvlib.core.inputs.vision.YuvToRgbConverter;
 import jp.oist.abcvlib.core.outputs.SocketListener;
 import jp.oist.abcvlib.util.ProcessPriorityThreadFactory;
 
-import static android.os.Process.THREAD_PRIORITY_DEFAULT;
-import static jp.oist.abcvlib.core.learning.flatbuffers.Color.Red;
-
-
 public class MainActivity extends AbcvlibActivity implements SocketListener {
 
     private TimeStepDataBuffer timeStepDataBuffer;
@@ -108,57 +104,20 @@ public class MainActivity extends AbcvlibActivity implements SocketListener {
 
     private void testFlatBuffers(){
         FlatBufferBuilder builder = new FlatBufferBuilder(1024);
-        int weaponOneName = builder.createString("Sword");
-        short weaponOneDamage = 3;
-        int weaponTwoName = builder.createString("Axe");
-        short weaponTwoDamage = 5;
-// Use the `createWeapon()` helper function to create the weapons, since we set every field.
-        int sword = Weapon.createWeapon(builder, weaponOneName, weaponOneDamage);
-        int axe = Weapon.createWeapon(builder, weaponTwoName, weaponTwoDamage);
 
-        // Serialize a name for our monster, called "Orc".
-        int name = builder.createString("Orc");
-// Create a `vector` representing the inventory of the Orc. Each number
-// could correspond to an item that can be claimed after he is slain.
-        byte[] treasure = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int inv = Monster.createInventoryVector(builder, treasure);
+        Episode.startEpisode(builder);
+        long[] timestamps = new long[]{1, 2, 3};
+        double[] left = new double[]{1,2,3};
+        double[] right = new double[]{1,2,3};
 
-        // Place the two weapons into an array, and pass it to the `createWeaponsVector()` method to
-// create a FlatBuffer vector.
-        int[] weaps = new int[2];
-        weaps[0] = sword;
-        weaps[1] = axe;
-// Pass the `weaps` array into the `createWeaponsVector()` method to create a FlatBuffer vector.
-        int weapons = Monster.createWeaponsVector(builder, weaps);
-        Monster.startPathVector(builder, 2);
-        Vec3.createVec3(builder, 1.0f, 2.0f, 3.0f);
-        Vec3.createVec3(builder, 4.0f, 5.0f, 6.0f);
-        int path = builder.endVector();
+        int ts = WheelCounts.createTimestampsVector(builder, timestamps);
+        int l = WheelCounts.createLeftVector(builder, left);
+        int r = WheelCounts.createRightVector(builder, right);
+        WheelCounts.createWheelCounts(builder, ts, l, r);
+        int episode = Episode.endEpisode(builder);
 
-        // Create our monster using `startMonster()` and `endMonster()`.
-        Monster.startMonster(builder);
-        Monster.addPos(builder, Vec3.createVec3(builder, 1.0f, 2.0f, 3.0f));
-        Monster.addName(builder, name);
-        Monster.addColor(builder, Red);
-        Monster.addHp(builder, (short)300);
-        Monster.addInventory(builder, inv);
-        Monster.addWeapons(builder, weapons);
-        Monster.addEquippedType(builder, Equipment.Weapon);
-        Monster.addEquipped(builder, axe);
-        Monster.addPath(builder, path);
-        int orc = Monster.endMonster(builder);
-
-        Monster.addEquippedType(builder, Equipment.Weapon); // Union type
-        // Call `finish()` to instruct the builder that this monster is complete.
-        builder.finish(orc); // You could also call `Monster.finishMonsterBuffer(builder, orc);`.
+        builder.finish(episode); // You could also call `Monster.finishMonsterBuffer(builder, orc);`.
         // This must be called after `finish()`.
-        Fbuf = builder.dataBuffer();
-        // This was causing bufferunderflow errors.
-//        byte[] fbufBytes = new byte[Fbuf.capacity()];
-//        Fbuf.get(fbufBytes);
-// The data in this ByteBuffer does NOT start at 0, but at buf.position().
-// The number of bytes is buf.remaining().
-// Alternatively this copies the above data out of the ByteBuffer for you:
         byteBuff = builder.sizedByteArray();
         Log.i("alskdjasd", "I'm a breakpoint!");
     }
