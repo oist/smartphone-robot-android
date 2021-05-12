65a8a47345c0eeebb599dc85ad49b9f744bd9802
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-05-12 12:39:22 +0900

    Added ImageOps class for common image operations

---
+++ libs/abcvlib/src/main/java/jp/oist/abcvlib/util/ImageOps.java
@@ -0,0 +1,18 @@
+package jp.oist.abcvlib.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class ImageOps {
+    public static Bitmap compressImage(Bitmap image, Bitmap.CompressFormat format, int quality){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(format, quality, baos);
+        int options = 100;
+        ByteArrayInputStream bais = new ByteArrayInputStream(
+                baos.toByteArray());
+        return BitmapFactory.decodeStream(bais, null, null);
+    }
+}
