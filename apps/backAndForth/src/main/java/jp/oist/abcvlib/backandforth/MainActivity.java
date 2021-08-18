a2b4b448805c23aed1425053b34262214bd36f19
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-08-18 12:16:26 +0900

    Completely rewrote backAndForth module. It was fighting MasterController rather than integrating with it.It was too complicated to show new users anything useful.

--- apps/backAndForth/src/main/java/jp/oist/abcvlib/backandforth/MainActivity.java
+++ apps/backAndForth/src/main/java/jp/oist/abcvlib/backandforth/MainActivity.java
@@ -1,16 +1,12 @@
 package jp.oist.abcvlib.backandforth;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.annotation.RequiresApi;
-
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import jp.oist.abcvlib.core.AbcvlibActivity;
 import jp.oist.abcvlib.core.IOReadyListener;
+import jp.oist.abcvlib.core.outputs.AbcvlibController;
 import jp.oist.abcvlib.util.ProcessPriorityThreadFactory;
 import jp.oist.abcvlib.util.ScheduledExecutorServiceWithException;
 
@@ -21,7 +17,8 @@ import jp.oist.abcvlib.util.ScheduledExecutorServiceWithException;
  */
 public class MainActivity extends AbcvlibActivity implements IOReadyListener {
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    private ScheduledExecutorServiceWithException executor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,71 +29,36 @@ public class MainActivity extends AbcvlibActivity implements IOReadyListener {
         // Setup Android GUI. Point this method to your main activity xml file or corresponding int
         // ID within the R class
         setContentView(R.layout.activity_main);
+        executor = new ScheduledExecutorServiceWithException(1,
+                new ProcessPriorityThreadFactory(Thread.NORM_PRIORITY, "BackAndForthController"));
     }
 
     @Override
     public void onIOReady() {
-        float speed = 1.0f;
-        float[][] speedProfile = {{speed, 0, -speed, 0}, {speed, 0, -speed, 0}, {2000, 1000, 2000, 1000}};
-        BackAndForth backAndForth = new BackAndForth(speedProfile);
-        backAndForth.start();
+        float speed = 0.5f;
+        BackAndForthController backAndForthController = new BackAndForthController(speed);
+
+        // Add the custom controller to the grand controller (controller that assembles other controllers)
+        getOutputs().getMasterController().addController(backAndForthController);
+
+        executor.scheduleAtFixedRate(backAndForthController, 0, 1000, TimeUnit.MILLISECONDS);
     }
 
-    class BackAndForth {
-
-        /**
-         * 3xn matrix with column c, row 1 representing speed of left wheel
-         * row 2 representing speed of right wheel, and row 3 the time window for
-         * for speed c. Speed specified as -1 to 1 representing maximum speed backward and forward.
-         * Time window specified in milliseconds
-          */
-        float[][] speedProfile;
-        ScheduledExecutorServiceWithException executor;
-        SpeedSetter speedSetter;
-
-        @RequiresApi(api = Build.VERSION_CODES.N)
-        public BackAndForth(float[][] speedProfile){
-            this.speedProfile = speedProfile;
-
-            executor = new ScheduledExecutorServiceWithException(1,
-                    new ProcessPriorityThreadFactory(Thread.NORM_PRIORITY, "BackAndForth"));
+    public static class BackAndForthController extends AbcvlibController {
+        float speed;
+        float currentSpeed;
+        public BackAndForthController(float speed){
+            this.speed = speed;
+            this.currentSpeed = speed;
         }
-
-        public void start(){
-            long totalTime = 0;
-            for (float timeLen : speedProfile[2]){
-                totalTime+=timeLen;
-            }
-            executor.scheduleAtFixedRate(() -> {
-                int endOfCurrentTimeSlot = 0;
-                for (int i = 0 ; i < speedProfile[0].length; i++){
-                    speedSetter = new SpeedSetter(speedProfile[0][i], speedProfile[1][i]);
-                    executor.schedule(speedSetter, endOfCurrentTimeSlot, TimeUnit.MILLISECONDS);
-                    endOfCurrentTimeSlot+=speedProfile[2][i];
-                }
-            }, 0, totalTime, TimeUnit.MILLISECONDS);
-        }
-    }
-
-    class SpeedSetter implements Runnable{
-
-        private final float left;
-        private final float right;
-
-        public SpeedSetter(float left, float right){
-            this.left = left;
-            this.right = right;
-        }
-
         @Override
         public void run() {
-            if (getOutputs() == null){
-                Log.d("backandforth", "Waiting for outputs to initialize");
+            if (currentSpeed == speed){
+                currentSpeed = -speed;
+            }else {
+                currentSpeed = speed;
             }
-            else{
-                getOutputs().setWheelOutput(left, right);
-                Log.i("BackAndForth", "LeftWheel = " + left + " RightWheel = " + right);
-            }
+            setOutput(currentSpeed, currentSpeed);
         }
     }
 }
