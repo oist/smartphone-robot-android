4349b98ecb9d6ba2f3d4923bdac4ac8b60f91d5b
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-08-19 12:37:40 +0900

    First working serviceTest

--- apps/serviceTest/src/main/java/jp/oist/abcvlib/servicetest/MyAbcvlibService.java
+++ apps/serviceTest/src/main/java/jp/oist/abcvlib/servicetest/MyAbcvlibService.java
@@ -16,10 +16,15 @@ import java.util.concurrent.TimeUnit;
 
 import jp.oist.abcvlib.core.AbcvlibService;
 import jp.oist.abcvlib.core.IOReadyListener;
+import jp.oist.abcvlib.core.outputs.AbcvlibController;
 import jp.oist.abcvlib.util.ProcessPriorityThreadFactory;
 import jp.oist.abcvlib.util.ScheduledExecutorServiceWithException;
 
 public class MyAbcvlibService extends AbcvlibService implements IOReadyListener {
+
+    ScheduledExecutorServiceWithException executor = new ScheduledExecutorServiceWithException(
+            1, new ProcessPriorityThreadFactory(Thread.NORM_PRIORITY,
+            "BackAndForthController"));
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -30,72 +35,29 @@ public class MyAbcvlibService extends AbcvlibService implements IOReadyListener 
 
     @Override
     public void onIOReady() {
-        float speed = 1.0f;
-        float[][] speedProfile = {{speed, 0, -speed, 0}, {speed, 0, -speed, 0}, {2000, 1000, 2000, 1000}};
-        BackAndForth backAndForth = new BackAndForth(speedProfile);
-        backAndForth.start();
+        float speed = 0.5f;
+        BackAndForthController backAndForthController = new BackAndForthController(speed);
+
+        // Add the custom controller to the grand controller (controller that assembles other controllers)
+        getOutputs().getMasterController().addController(backAndForthController);
+        executor.scheduleAtFixedRate(backAndForthController, 0, 1000, TimeUnit.MILLISECONDS);
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    class BackAndForth {
-        /**
-         * 3xn matrix with column c, row 1 representing speed of left wheel
-         * row 2 representing speed of right wheel, and row 3 the time window for
-         * for speed c. Speed specified as -1 to 1 representing maximum speed backward and forward.
-         * Time window specified in milliseconds
-         */
-        float[][] speedProfile;
-        ScheduledExecutorServiceWithException executor;
-        SpeedSetter speedSetter;
-
-        @RequiresApi(api = Build.VERSION_CODES.N)
-        public BackAndForth(float[][] speedProfile){
-            this.speedProfile = speedProfile;
-
-            executor = new ScheduledExecutorServiceWithException(1,
-                    new ProcessPriorityThreadFactory(Thread.NORM_PRIORITY, "BackAndForth"));
+    public static class BackAndForthController extends AbcvlibController {
+        float speed;
+        float currentSpeed;
+        public BackAndForthController(float speed){
+            this.speed = speed;
+            this.currentSpeed = speed;
         }
-
-        public void start(){
-            long totalTime = 0;
-            for (float timeLen : speedProfile[2]){
-                totalTime+=timeLen;
-            }
-            executor.scheduleAtFixedRate(() -> {
-                int endOfCurrentTimeSlot = 0;
-                for (int i = 0 ; i < speedProfile[0].length; i++){
-                    speedSetter = new SpeedSetter(speedProfile[0][i], speedProfile[1][i]);
-                    executor.schedule(speedSetter, endOfCurrentTimeSlot, TimeUnit.MILLISECONDS);
-                    endOfCurrentTimeSlot+=speedProfile[2][i];
-                }
-            }, 0, totalTime, TimeUnit.MILLISECONDS);
-        }
-    }
-
-    class SpeedSetter implements Runnable{
-
-        private final float left;
-        private final float right;
-
-        public SpeedSetter(float left, float right){
-            this.left = left;
-            this.right = right;
-        }
-
         @Override
         public void run() {
-            if (getOutputs() == null){
-                Log.d("backandforth", "Waiting for outputs to initialize");
+            if (currentSpeed == speed){
+                currentSpeed = -speed;
+            }else {
+                currentSpeed = speed;
             }
-            else{
-                getOutputs().setWheelOutput(left, right);
-                Log.i("BackAndForth", "LeftWheel = " + left + " RightWheel = " + right);
-            }
+            setOutput(currentSpeed, currentSpeed);
         }
     }
 }
--- libs/abcvlib/src/main/java/jp/oist/abcvlib/core/AbcvlibService.java
+++ libs/abcvlib/src/main/java/jp/oist/abcvlib/core/AbcvlibService.java
@@ -1,19 +1,31 @@
 package jp.oist.abcvlib.core;
 
+import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
+
+import com.permissioneverywhere.PermissionEverywhere;
+import com.permissioneverywhere.PermissionResponse;
+import com.permissioneverywhere.PermissionResultCallback;
 
 import java.util.Iterator;
 import java.util.Map;
 
 import ioio.lib.util.IOIOLooper;
-import ioio.lib.util.android.IOIOActivity;
+import ioio.lib.util.android.IOIOService;
 import jp.oist.abcvlib.core.inputs.Inputs;
 import jp.oist.abcvlib.core.outputs.Outputs;
 
@@ -28,8 +40,7 @@ import jp.oist.abcvlib.core.outputs.Outputs;
  * @author Christopher Buckley https://github.com/topherbuckley
  *
  */
-public abstract class AbcvlibService extends IOIOActivity {
-
+public abstract class AbcvlibService extends IOIOService implements AbcvlibAbstractObject {
     // Publically accessible objects that encapsulate a lot other core functionality
     private Inputs inputs;
     private Outputs outputs;
@@ -37,38 +48,73 @@ public abstract class AbcvlibService extends IOIOActivity {
     private AbcvlibLooper abcvlibLooper;
     private static final String TAG = "abcvlib";
     private IOReadyListener ioReadyListener;
+    private final String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
 
     public void setIoReadyListener(IOReadyListener ioReadyListener) {
         this.ioReadyListener = ioReadyListener;
     }
 
-    protected void onCreate(Bundle savedInstanceState) {
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        NotificationManager notificationManager = (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        if (intent != null && intent.getAction() != null
+                && intent.getAction().equals("stop")) {
+            // User clicked the notification. Need to stop the service.
+            if (notificationManager != null) {
+                notificationManager.cancel(0);
+            }
+            stopSelf();
+        } else {
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getBaseContext());
+
+            notificationBuilder
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setTicker("IOIO service running")
+                    .setWhen(System.currentTimeMillis())
+                    .setContentTitle("IOIO Service")
+                    .setContentText("Click to stop")
+                    .setContentIntent(PendingIntent.getService(this, 0, new Intent(
+                            "stop", null, this, this.getClass()), 0));
+
+            if (notificationManager != null) {
+                notificationManager.notify(1, notificationBuilder.build());
+            }
+        }
+
         inputs = new Inputs(getApplicationContext());
-        /*
-        This much be called prior to initializing outputs as this is what triggers the creation
-        of the abcvlibLooper instance passed to the Outputs constructor
-        */
-        super.onCreate(savedInstanceState);
+        int result = super.onStartCommand(intent, flags, startId);
+        return result;
+    }
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        Log.v(TAG, "End of AbcvlibActivity.onCreate");
+    public void checkPermissions(){
+        PermissionEverywhere.getPermission(getApplicationContext(),
+                new String[]{Manifest.permission.CAMERA},
+                1,
+                "Notification title",
+                "This app needs a camera permission",
+                R.mipmap.ic_launcher)
+                .enqueue(new PermissionResultCallback() {
+                    @Override
+                    public void onComplete(PermissionResponse permissionResponse) {
+                        Toast.makeText(AbcvlibService.this, "is Granted " + permissionResponse.isGranted(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    public void onDestroy() {
         abcvlibLooper.setDutyCycle(0, 0);
-        Log.v(TAG, "End of AbcvlibActivity.onStop");
+        Log.v(TAG, "AbcvlibService onDestroy");
+        super.onDestroy();
     }
 
     @Override
-    public void onPause()
-    {
-        super.onPause();
-        abcvlibLooper.setDutyCycle(0, 0);
-        Log.i(TAG, "End of AbcvlibActivity.onPause");
-    }
-
     public Inputs getInputs() {
         return inputs;
     }
@@ -85,41 +131,41 @@ public abstract class AbcvlibService extends IOIOActivity {
         outputs = new Outputs(switches, abcvlibLooper, inputs);
     }
 
-    /**
-     * Take an array of permissions and check if they've all been granted. If not, request them. If
-     * denied close app.
-     * @param permissionsListener: Typically your main activity, where you'd implement this interface
-     *                           and put all your main code into the {@link PermissionsListener#onPermissionsGranted()}
-     * @param permissions: list of {@link android.Manifest.permission} strings your app requires.
-     */
-    protected void checkPermissions(PermissionsListener permissionsListener, String[] permissions){
-        boolean permissionsGranted = true;
-        for (String permission:permissions){
-            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED){
-                permissionsGranted = false;
-            }
-        }
-        if (permissionsGranted) {
-            permissionsListener.onPermissionsGranted();
-        } else {
-            ActivityResultLauncher<String[]> requestPermissionLauncher =
-                    registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
-                        Iterator<Map.Entry<String, Boolean>> iterator = isGranted.entrySet().iterator();
-                        boolean allGranted = false;
-                        while(iterator.hasNext()){
-                            Map.Entry<String, Boolean> pair = iterator.next();
-                            allGranted = pair.getValue();
-                        }
-                        if (allGranted) {
-                            Log.i(TAG, "Permissions granted");
-                            permissionsListener.onPermissionsGranted();
-                        } else {
-                            throw new RuntimeException("You did not approve required permissions");
-                        }
-                    });
-            requestPermissionLauncher.launch(permissions);
-        }
-    }
+//    /**
+//     * Take an array of permissions and check if they've all been granted. If not, request them. If
+//     * denied close app.
+//     * @param permissionsListener: Typically your main activity, where you'd implement this interface
+//     *                           and put all your main code into the {@link PermissionsListener#onPermissionsGranted()}
+//     * @param permissions: list of {@link android.Manifest.permission} strings your app requires.
+//     */
+//    protected void checkPermissions(PermissionsListener permissionsListener, String[] permissions){
+//        boolean permissionsGranted = true;
+//        for (String permission:permissions){
+//            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED){
+//                permissionsGranted = false;
+//            }
+//        }
+//        if (permissionsGranted) {
+//            permissionsListener.onPermissionsGranted();
+//        } else {
+//            ActivityResultLauncher<String[]> requestPermissionLauncher =
+//                    registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), isGranted -> {
+//                        Iterator<Map.Entry<String, Boolean>> iterator = isGranted.entrySet().iterator();
+//                        boolean allGranted = false;
+//                        while(iterator.hasNext()){
+//                            Map.Entry<String, Boolean> pair = iterator.next();
+//                            allGranted = pair.getValue();
+//                        }
+//                        if (allGranted) {
+//                            Log.i(TAG, "Permissions granted");
+//                            permissionsListener.onPermissionsGranted();
+//                        } else {
+//                            throw new RuntimeException("You did not approve required permissions");
+//                        }
+//                    });
+//            requestPermissionLauncher.launch(permissions);
+//        }
+//    }
 
     /**
      Overriding here passes the initialized AbcvlibLooper object to the IOIOLooper class which
