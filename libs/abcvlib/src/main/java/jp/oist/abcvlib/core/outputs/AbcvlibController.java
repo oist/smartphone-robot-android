14e3574bddecc1501746a08dfd64f3c1a7058444
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-08-23 12:04:35 +0900

    Replaced builder subclass with abstract AbcvlibController architecture.
    Connecting with Master Controller now required by implementer of custom controller as it ended up costing the same amount of lines of code if abstracted

--- libs/abcvlib/src/main/java/jp/oist/abcvlib/core/outputs/AbcvlibController.java
+++ libs/abcvlib/src/main/java/jp/oist/abcvlib/core/outputs/AbcvlibController.java
@@ -2,12 +2,11 @@ package jp.oist.abcvlib.core.outputs;
 
 import java.util.concurrent.TimeUnit;
 
-import jp.oist.abcvlib.core.AbcvlibAbstractObject;
 import jp.oist.abcvlib.util.ErrorHandler;
 import jp.oist.abcvlib.util.ProcessPriorityThreadFactory;
 import jp.oist.abcvlib.util.ScheduledExecutorServiceWithException;
 
-public class AbcvlibController implements Runnable{
+public abstract class AbcvlibController implements Runnable{
 
     private String name;
     private int threadCount = 1;
@@ -17,30 +16,44 @@ public class AbcvlibController implements Runnable{
     private TimeUnit timeUnit;
     private ScheduledExecutorServiceWithException executor;
     private final String TAG = getClass().getName();
-    private AbcvlibController childController;
 
     public AbcvlibController(){}
 
-    public AbcvlibController(AbcvlibAbstractObject abcvlibAbstractObject,
-                             AbcvlibController childController,String name,
-                                    int threadCount, int threadPriority, int initDelay,
-                                    int timeStep, TimeUnit timeUnit){
-        // Add the custom controller to the grand controller (controller that assembles other controllers)
-        abcvlibAbstractObject.getOutputs().getMasterController().addController(childController);
-        this.childController = childController;
+    public AbcvlibController setName(String name) {
         this.name = name;
+        return this;
+    }
+
+    public AbcvlibController setThreadCount(int threadCount) {
         this.threadCount = threadCount;
-        this.threadPriority  = threadPriority;
+        return this;
+    }
+
+    public AbcvlibController setThreadPriority(int threadPriority) {
+        this.threadPriority = threadPriority;
+        return this;
+    }
+
+    public AbcvlibController setInitDelay(int initDelay) {
         this.initDelay = initDelay;
+        return this;
+    }
+
+    public AbcvlibController setTimestep(int timeStep) {
         this.timeStep = timeStep;
+        return this;
+    }
+
+    public AbcvlibController setTimeUnit(TimeUnit timeUnit) {
         this.timeUnit = timeUnit;
-        this.executor = new ScheduledExecutorServiceWithException(
-                threadCount, new ProcessPriorityThreadFactory(threadPriority,
-                name));
+        return this;
     }
 
     public void startController(){
-        executor.scheduleAtFixedRate(childController, initDelay, timeStep, timeUnit);
+        executor = new ScheduledExecutorServiceWithException(
+                threadCount, new ProcessPriorityThreadFactory(threadPriority,
+                name));
+        executor.scheduleAtFixedRate(this, initDelay, timeStep, timeUnit);
     }
 
     public void stopController(){
@@ -66,61 +79,5 @@ public class AbcvlibController implements Runnable{
     public static class Output{
         public float left;
         public float right;
-    }
-
-    public static class AbcvlibControllerBuilder {
-        private final AbcvlibController abcvlibController;
-        private final AbcvlibAbstractObject abcvlibAbstractObject;
-        private String name;
-        private int threadCount = 1;
-        private int threadPriority = Thread.NORM_PRIORITY;
-        private int initDelay = 0;
-        private int timeStep = 1;
-        private TimeUnit timeUnit = TimeUnit.SECONDS;
-
-        /**
-         * @param abcvlibAbstractObject Typically the keyword "this" if calling from a child class of AbcvlibActivity or AbcvlibService
-         * @param abcvlibController The custom AbcvlibController you have defined.
-         */
-        public AbcvlibControllerBuilder(AbcvlibAbstractObject abcvlibAbstractObject,
-                                        AbcvlibController abcvlibController) {
-            this.abcvlibController = abcvlibController;
-            this.abcvlibAbstractObject = abcvlibAbstractObject;
-        }
-
-        public AbcvlibController build() {
-            return new AbcvlibController(abcvlibAbstractObject, abcvlibController, name,
-            threadCount, threadPriority, initDelay, timeStep, timeUnit);
-        }
-
-        public AbcvlibControllerBuilder setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public AbcvlibControllerBuilder setThreadCount(int threadCount) {
-            this.threadCount = threadCount;
-            return this;
-        }
-
-        public AbcvlibControllerBuilder setThreadPriority(int threadPriority) {
-            this.threadPriority = threadPriority;
-            return this;
-        }
-
-        public AbcvlibControllerBuilder setInitDelay(int initDelay) {
-            this.initDelay = initDelay;
-            return this;
-        }
-
-        public AbcvlibControllerBuilder setTimestep(int timeStep) {
-            this.timeStep = timeStep;
-            return this;
-        }
-
-        public AbcvlibControllerBuilder setTimeUnit(TimeUnit timeUnit) {
-            this.timeUnit = timeUnit;
-            return this;
-        }
     }
 }
