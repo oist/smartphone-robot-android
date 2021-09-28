3e8f11f8f215b5a1c6bdb6986f1fb471090956e3
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-09-28 17:29:47 +0900

    Replaced CustomController with updated version depending on realistic speeds

--- apps/pidtransfer_transmitter/src/main/java/jp/oist/abcvlib/pidtransfer_transmitter/CustomController.java
+++ apps/pidtransfer_transmitter/src/main/java/jp/oist/abcvlib/pidtransfer_transmitter/CustomController.java
@@ -2,30 +2,34 @@ package jp.oist.abcvlib.pidtransfer_transmitter;
 
 import android.util.Log;
 
-import org.json.JSONException;
-
-import jp.oist.abcvlib.core.AbcvlibActivity;
+import jp.oist.abcvlib.core.inputs.microcontroller.WheelDataSubscriber;
 import jp.oist.abcvlib.core.outputs.AbcvlibController;
 
-public class CustomController extends AbcvlibController {
+/**
+ * Simple proportional controller trying to achieve some setSpeed set by python server GUI.
+ */
+public class CustomController extends AbcvlibController implements WheelDataSubscriber {
 
-    private final AbcvlibActivity abcvlibActivity;
+    double actualSpeed = 0;
+    double errorSpeed = 0;
+    double maxSpeed = 350; // Just spot measurede this by setOutput(1.0, 1.0) and read the log. This will surely change with battery level and wheel wear/tear.
+    double setSpeed = 50; // mm/s.
+    double d_s = 0.0001; // derivative controller for speed of wheels
 
-    public CustomController(AbcvlibActivity abcvlibActivity){
-        this.abcvlibActivity = abcvlibActivity;
+    public CustomController(){}
+
+    public void run(){
+        errorSpeed = setSpeed - actualSpeed;
+
+        // Note the use of the same output for controlling both wheels. Due to various errors
+        // that build up over time, controling individual wheels has so far led to chaos
+        // and unstable controllers.
+        setOutput((float) ((setSpeed / maxSpeed) + ((errorSpeed * d_s) / maxSpeed)), (float) ((setSpeed / maxSpeed) + ((errorSpeed * d_s) / maxSpeed)));
     }
 
     @Override
-    public void run() {
-
-        while (!abcvlibActivity.appRunning){
-            Log.i("abcvlib", this.toString() + "Waiting for appRunning to be true");
-            Thread.yield();
-        }
-
-        // Turn right slowly
-        setOutput(-15, 15);
-
-        }
+    public void onWheelDataUpdate(long timestamp, int wheelCountL, int wheelCountR, double wheelDistanceL, double wheelDistanceR, double wheelSpeedInstantL, double wheelSpeedInstantR, double wheelSpeedBufferedL, double wheelSpeedBufferedR, double wheelSpeedExpAvgL, double wheelSpeedExpAvgR) {
+        actualSpeed = wheelSpeedBufferedL;
+        Log.d("WheelUpdate", "wheelSpeedBufferedL: " + wheelSpeedBufferedL + ", wheelSpeedBufferedR: " + wheelSpeedBufferedR);
     }
-
+}
