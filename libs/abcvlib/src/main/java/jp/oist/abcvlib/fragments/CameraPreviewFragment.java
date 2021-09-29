2dbcc8d2fdf3c0152329865ceb59a3f1960de06c
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2021-09-29 18:01:34 +0900

    Added a CameraPreviewFragment

---
+++ libs/abcvlib/src/main/java/jp/oist/abcvlib/fragments/CameraPreviewFragment.java
@@ -0,0 +1,15 @@
+package jp.oist.abcvlib.fragments;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+
+import jp.oist.abcvlib.core.R;
+import jp.oist.abcvlib.core.inputs.phone.ImageDataSubscriber;
+
+public class CameraPreviewFragment extends Fragment {
+
+    public CameraPreviewFragment() {
+        super(R.layout.camera_x_preview);
+    }
+}
