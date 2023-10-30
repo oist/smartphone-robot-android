7871f9236d864762d47fcfa8f11dae1fb8601190
Christopher Buckley <topherbuckley@gmail.com>
2023-10-30 14:37:08 +0100

    Removing phonesideUSB from rp2040_state

--- libs/abcvlib/src/main/java/jp/oist/abcvlib/util/RP2040State.java
+++ libs/abcvlib/src/main/java/jp/oist/abcvlib/util/RP2040State.java
@@ -41,24 +41,19 @@ public class RP2040State {
             return ((float) voltage / 1000f);
         }
     }
-    //    CHARGE_SIDE_USB((byte) 0x01), Mux MAX77976 and NCP3901 Data: Charger-side USB Voltage, and Wireless Coil State
-//    PHONE_SIDE_USB((byte) 0x0C), // MAX77958 Phone-side USB Controller
     protected class ChargeSideUSB{
         int max77976_chg_details;
         boolean ncp3901_wireless_charger_attached;
         short usb_charger_voltage;
     }
-    protected class PhoneSideUSB{}
 
     MotorsState motorsState;
     BatteryDetails batteryDetails;
     ChargeSideUSB chargeSideUSB;
-    PhoneSideUSB phoneSideUSB;
 
     public RP2040State(){
         motorsState = new MotorsState();
         batteryDetails = new BatteryDetails();
         chargeSideUSB = new ChargeSideUSB();
-        phoneSideUSB = new PhoneSideUSB();
     }
 }
