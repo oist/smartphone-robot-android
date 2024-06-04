65a490477b9f10c2ec255c40cd8d350215c6844c
Christopher Buckley <topherbuckley@gmail.com>
2024-06-04 16:21:34 +0900

    wip

--- .github/scripts/generate_output_paths.sh
+++ .github/scripts/generate_output_paths.sh
@@ -3,9 +3,7 @@
 # Extract the app names
 apps=$(jq -r '.apps | join(",")' apps-config.json)
 
-# Create the debug and release lines
-debug_line="apps/{$apps}/build/outputs/apk/debug/{$apps}-debug.apk"
-release_line="apps/{$apps}/build/outputs/apk/release/{$apps}-release-unsigned.apk"
-
-# Print the lines
-echo -e "$debug_line\n$release_line"
+for app in $(echo $apps | tr "," "\n"); do
+  echo "./apps/${app}/build/outputs/apk/debug/${app}-debug.apk"
+  echo "./apps/${app}/build/outputs/apk/release/${app}-release-unsigned.apk"
+done
