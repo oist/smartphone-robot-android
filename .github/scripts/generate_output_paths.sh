6c4d45e7afe9563cdb2acdaeebffa494a2f2ceff
Christopher Buckley <topherbuckley@gmail.com>
2024-06-04 15:26:40 +0900

    Updating automated releases

--- .github/scripts/generate_output_paths.sh
+++ .github/scripts/generate_output_paths.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
-files=$(jq -r '.apps[] | "./apps/" + . + "/build/outputs/apk/debug/" + . + "-debug.apk ./apps/" + . + "/build/outputs/apk/release/" + . + "-release.apk"' apps-config.json)
-echo "files=$files" >> $GITHUB_ENV
+#!/bin/bash
+
+# Read the JSON file
+json=$(cat your_file.json)
+
+# Extract the app names
+apps=$(echo "$json" | jq -r '.apps | join(",")')
+
+# Create the debug and release lines
+debug_line="apps/{$apps}/build/outputs/apk/debug/{$apps}-debug.apk"
+release_line="apps/{$apps}/build/outputs/apk/release/{$apps}-release-unsigned.apk"
+
+# Print the lines
+echo -e "$debug_line\n$release_line"
