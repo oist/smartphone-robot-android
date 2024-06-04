ae898f01d64119dd79a3ffddf2e20b2aa9a3403f
Christopher Buckley <topherbuckley@gmail.com>
2024-06-04 15:32:26 +0900

    wip

--- .github/scripts/generate_output_paths.sh
+++ .github/scripts/generate_output_paths.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Read the JSON file
-json=$(cat your_file.json)
-
 # Extract the app names
-apps=$(echo "$json" | jq -r '.apps | join(",")')
+apps=$(jq -r '.apps | join(",")' apps-config.json)
 
 # Create the debug and release lines
 debug_line="apps/{$apps}/build/outputs/apk/debug/{$apps}-debug.apk"
