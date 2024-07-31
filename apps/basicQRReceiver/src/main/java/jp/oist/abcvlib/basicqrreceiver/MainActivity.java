f4974d1eed8b45c4cfe9fe2189bfed937da4e8b6
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2024-07-31 23:18:36 +0900

    Adding basicQRReceiver

--- apps/basicQRReceiver/src/main/java/jp/oist/abcvlib/basicqrreceiver/MainActivity.java
+++ apps/basicQRReceiver/src/main/java/jp/oist/abcvlib/basicqrreceiver/MainActivity.java
@@ -1,18 +1,13 @@
-package jp.oist.abcvlib.basicqrtransmitter;
+package jp.oist.abcvlib.basicqrreceiver;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import java.util.concurrent.TimeUnit;
+import android.widget.TextView;
 
 import jp.oist.abcvlib.core.AbcvlibActivity;
 import jp.oist.abcvlib.core.inputs.PublisherManager;
-import jp.oist.abcvlib.core.inputs.phone.ImageDataRaw;
 import jp.oist.abcvlib.core.inputs.phone.QRCodeData;
 import jp.oist.abcvlib.core.inputs.phone.QRCodeDataSubscriber;
-import jp.oist.abcvlib.util.ProcessPriorityThreadFactory;
 import jp.oist.abcvlib.util.QRCode;
-import jp.oist.abcvlib.util.ScheduledExecutorServiceWithException;
 import jp.oist.abcvlib.util.SerialCommManager;
 import jp.oist.abcvlib.util.SerialReadyListener;
 import jp.oist.abcvlib.util.UsbSerial;
@@ -30,10 +25,9 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
     private PublisherManager publisherManager;
     private float speedL = 0;
     private float speedR = 0;
-    private float speed = 0.6f;
-    private String qrcodeData = "";
-    private enum ACTIONS {TURN_LEFT, TURN_RIGHT};
-    private ACTIONS action = ACTIONS.TURN_RIGHT;
+    private float speed = 0;
+    TextView letterTextView;
+
 
     public MainActivity() {
     }
@@ -44,26 +38,12 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
         // Passes Android App information up to parent classes for various usages. Do not modify
         super.onCreate(savedInstanceState);
 
+        letterTextView = findViewById(R.id.letterTextView);
+
         // Setup Android GUI. Point this method to your main activity xml file or corresponding int
         // ID within the R class
         setContentView(R.layout.activity_main);
-
-        // create a new QRCode object with input args point to the FragmentManager and your layout fragment where you want to generate the qrcode image.
-        qrCode = new QRCode(getSupportFragmentManager(), R.id.qrFragmentView);
     }
-
-    private Runnable swapAction = new Runnable() {
-        @Override
-        public void run() {
-            if (action == ACTIONS.TURN_RIGHT){
-                action = ACTIONS.TURN_LEFT;
-                turnRight();
-            } else {
-                action = ACTIONS.TURN_RIGHT;
-                turnLeft();
-            }
-        }
-    };
 
     @Override
     public void onSerialReady(UsbSerial usbSerial) {
@@ -83,14 +63,14 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
     public void onOutputsReady() {
         publisherManager.initializePublishers();
         publisherManager.startPublishers();
-        ScheduledExecutorServiceWithException executor = new ScheduledExecutorServiceWithException(1, new ProcessPriorityThreadFactory(Thread.MIN_PRIORITY, "ActionSelector"));
-        executor.scheduleAtFixedRate(swapAction, 0, 10, TimeUnit.SECONDS);
     }
 
     @Override
     public void onQRCodeDetected(String qrDataDecoded) {
-        if (!qrDataDecoded.equals("")){
-            Log.i("qrcode", "QR Code Found and decoded: " + qrDataDecoded);
+        if (!qrDataDecoded.equals("L")){
+            turnLeft();
+        } else if (!qrDataDecoded.equals("R")){
+            turnRight();
         }
     }
 
@@ -103,17 +83,12 @@ public class MainActivity extends AbcvlibActivity implements SerialReadyListener
     private void turnRight(){
         speedL = -speed;
         speedR = speed;
-        qrCodeRegenerate("R");
+        letterTextView.setText("R");
     }
 
     private void turnLeft(){
         speedL = speed;
         speedR = -speed;
-        qrCodeRegenerate("L");
-    }
-
-    private void qrCodeRegenerate(String qrcodeData){
-        qrCode.close();
-        qrCode.generate(qrcodeData);
-    }
+        letterTextView.setText("L");
+        }
 }
